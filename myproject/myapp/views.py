752e65d75d5a192243bbb664d1de2d80f151be94
root <root@ip-172-31-37-153.us-west-2.compute.internal>
2026-03-12 08:19:01 +0000

    pushing working django contents

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,7 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 def home(request):
     return HttpResponse("Hello, this is a sample Django project!")
 
+
+# Create your views here.
